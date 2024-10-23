e6dbe308b186aedb0e6e44f5402622fb6d89cc20
JeremyGelb <gelbjeremy22@gmail.com>
2024-10-22 20:17:21 -0400

    reworking how the c++ include is done with boost

--- src/spatial_indexing.h
+++ src/spatial_indexing.h
@@ -6,35 +6,8 @@
 #define spatial_index_H
 
 #include "spNetwork.h"
+#include "boost_importer.h"
 
-// some boost libraries used to building an rtree
-#include <b/geometry.hpp>
-#include <b/geometry/geometries/geometries.hpp>
-#include <b/geometry/geometries/point.hpp>
-#include <b/geometry/geometries/box.hpp>
-#include <b/geometry/index/rtree.hpp>
-#include <b/geometry/algorithms/distance.hpp>
-#include <b/foreach.hpp>
-#include <b/geometry/index/predicates.hpp>
-
-// **** some namespaces related to boost ****
-namespace bg = boost::geometry;
-namespace bgi = boost::geometry::index;
-
-// ***** some global types used ****
-
-// basic geometries
-typedef bg::model::d2::point_xy<double> point_t;
-typedef bg::model::linestring<point_t> linestring_t;
-typedef bg::model::box<point_t> bbox;
-
-// a simple vector of boost lines
-typedef std::vector<linestring_t> lines_vector;
-
-// some simple types for rtree
-typedef std::pair<bbox, int> rtree_element;
-typedef bgi::rtree< rtree_element, bgi::quadratic<16>> MyRtree;
-typedef std::vector<rtree_element> vector_rtree_element ;
 
 //******************************************************
 // ***** RTREE CLASS **** //
